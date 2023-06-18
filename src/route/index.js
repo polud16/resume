f2959b26af5b0adf60e8f9f8b93975f198be20d2
polud16 <booogdan1603@gmail.com>
2023-06-18 13:48:01 +0300

    bootstrap словник

--- src/route/index.js
+++ src/route/index.js
@@ -1124,5 +1124,17 @@ router.get('/facebook', function (req, res) {
 
 // ================================================================
 
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
